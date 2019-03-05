fbb00a97668c4656f6372be198d93ac4bf737910
Spencer Kittleson <spencerkittleson@hotmail.com>
2019-03-04 20:50:07 -0800

    Added json file site builder

--- utils.js
+++ utils.js
@@ -2,21 +2,56 @@ const handlebars = require("handlebars");
 const path = require("path");
 const fs = require("fs");
 
-
 /**
  * inFile - string
  * outFile - string
  * data - object for handlebars
  */
-module.exports.saveHandlebarsToHtml = function saveHandlebarsToHtml(
-  inFile,
-  outFile,
-  data
-) {
+function saveHandlebarsToHtml(inFile, outFile, data) {
   const source = fs.readFileSync(inFile, "utf8");
   const template = handlebars.compile(source, { strict: true });
   ensureDirectoryExistence(outFile);
   fs.writeFileSync(outFile, template(data), {});
+}
+
+module.exports.buildSiteFromJson = function buildSiteFromJson(json, src, dest) {
+  var store = JSON.parse(json);
+  if (!store.site) {
+    throw new Error("Site property must be in file.");
+  }
+  if (!store.pages && Array.isArray(store.pages)) {
+    throw new Error("Site pages must be an array");
+  }
+  let partials = [];
+  store.partials.forEach(partial => {
+    const partialName = partial.split("/")[1].split(".")[0];
+    partials.push({ name: partialName, path: `${src}/${partial}` });
+  });
+  registerPartials(partials);
+  store.pages.forEach(page => {
+    const pageKeyValue = Object.assign(store.site, page);
+    const folder = page.file.replace(".hbs", "");
+    let saveToPath = `${dest}/${folder}/index.html`;
+    if (page.file === "index.hbs") {
+      saveToPath = `${dest}/index.html`;
+    }
+    saveHandlebarsToHtml(`${src}/${page.file}`, saveToPath, pageKeyValue);
+  });
+
+  let siteMap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.schema.org/schemas/sitemap/0.9">`;
+  store.pages.forEach(page => {
+    let folder = page.file.replace(".hbs", "");
+    let priority = 0.8;
+    if (page.file === "index.hbs") {
+      priority = 1.0;
+      folder = "";
+    }
+    siteMap += `<url><loc>${
+      store.site.url
+    }/${folder}</loc><priority>${priority}</priority></url>`;
+  });
+  siteMap += "</urlset>";
+  fs.writeFileSync(`${dest}/sitemap.xml`, siteMap);
 };
 
 function ensureDirectoryExistence(filePath) {
@@ -28,7 +63,7 @@ function ensureDirectoryExistence(filePath) {
   fs.mkdirSync(dirname);
 }
 
-module.exports.registerPartials = function(partials){
+function registerPartials(partials) {
   partials.forEach(partial => {
     handlebars.registerPartial(
       partial.name,
